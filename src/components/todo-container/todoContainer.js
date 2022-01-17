c5d6cced8ee9440e649066619ece874670698f86
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-01-17 17:58:42 +0600

    Add basic folder structure for components

---
+++ src/components/todo-container/todoContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './todo-container.scss';
+
+class TodoContainer extends React.Component {
+  render() {
+    return <div>todo-container</div>;
+  }
+}
+
+export default TodoContainer;
