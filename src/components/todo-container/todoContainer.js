454c2bbd5cdc111c7cc370a33d43c179b8ba1d3c
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-01-18 15:59:02 +0600

    Add handleSubmit

--- src/components/todo-container/todoContainer.js
+++ src/components/todo-container/todoContainer.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './todo-container.scss';
+import { v4 as uuidv4 } from 'uuid';
 
 import TodoList from '../todo-list/todoList';
 import Header from '../header/header';
+import InputTodo from '../input-todo/inputTodo';
 
 class TodoContainer extends React.Component {
   constructor(props) {
@@ -10,17 +12,17 @@ class TodoContainer extends React.Component {
     this.state = {
       todos: [
         {
-          id: 1,
+          id: uuidv4(),
           title: 'Setup development environment',
           completed: true,
         },
         {
-          id: 2,
+          id: uuidv4(),
           title: 'Develop website and add content',
           completed: false,
         },
         {
-          id: 3,
+          id: uuidv4(),
           title: 'Deploy to live server',
           completed: false,
         },
@@ -52,10 +54,21 @@ class TodoContainer extends React.Component {
     });
   };
 
+  addTodoItem = (title) => {
+    const newTodo = {
+      title,
+      id: uuidv4(),
+      completed: false,
+    };
+
+    this.setState({ todos: [...this.state.todos, newTodo] });
+  };
+
   render() {
     return (
       <div>
         <Header />
+        <InputTodo addTodoItem={this.addTodoItem} />
         <TodoList
           handleChange={this.handleChange}
           delTodo={this.delTodo}
