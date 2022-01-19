38f484936d48294d8e90c996eb3c354b9fb47838
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-01-19 20:47:32 +0600

    Change router options

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as BrowserRouter } from 'react-router-dom';
 
 import TodoContainer from './components/todo-container/todoContainer';
 
